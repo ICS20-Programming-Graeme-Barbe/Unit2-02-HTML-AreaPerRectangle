20700a8a942e4f7d0f28addd5a133b49ba9e0b15
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-03-19 18:56:22 +0000

    index2.html

---
+++ js/script2.js
@@ -0,0 +1,9 @@
+// Created by: Graeme
+// Created on: March  19 
+// This file contains the JS functions for index2.html
+
+// Function for math
+function ButtonClicked () {
+	document.getElementById('add-math').innerHTML = 'The perimiter is ' + (3 + 3 + 5 + 5)
+	document.getElementById('multiply-math').innerHTML = 'The area is ' + (3 * 5)
+}
